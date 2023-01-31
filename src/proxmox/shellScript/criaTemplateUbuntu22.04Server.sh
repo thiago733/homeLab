fde6946e09981dab1eaa920968f4b79ae7fb31b9
thiago733 <thiago733@gmail.com>
2023-01-31 14:56:16 -0400

    ajustando tamanho do disco

--- src/proxmox/shellScript/criaTemplateUbuntu22.04Server.sh
+++ src/proxmox/shellScript/criaTemplateUbuntu22.04Server.sh
@@ -4,6 +4,7 @@ ID_VIRTUALMACHINE=7000
 IMAGEM=jammy-server-cloudimg-amd64.img
 MEMORIA_RAM=2048
 NOME_DO_TEMPLATE=UbuntuServer2204
+TAMANHO_DISCO=+4G
 URL=https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
 VCORE=2
 
@@ -12,7 +13,7 @@ wget $IMAGEM $URL
 qm create $ID_VIRTUALMACHINE --cores $VCORE --memory $MEMORIA_RAM --name $NOME_DO_TEMPLATE --net0 virtio,bridge=vmbr0
 qm importdisk $ID_VIRTUALMACHINE $IMAGEM local-btrfs
 qm set $ID_VIRTUALMACHINE --scsihw virtio-scsi-pci --scsi0 local-btrfs:$ID_VIRTUALMACHINE/vm-$ID_VIRTUALMACHINE-disk-0.raw
-qm set $ID_VIRTUALMACHINE --ide2 local-btrfs:cloudinit
+qm set $ID_VIRTUALMACHINE --ide2 size=4GB local-btrfs:cloudinit
 qm set $ID_VIRTUALMACHINE --boot c --bootdisk scsi0
 qm set $ID_VIRTUALMACHINE --serial0 socket --vga serial0
 qm set $ID_VIRTUALMACHINE --agent 1
@@ -20,4 +21,5 @@ qm set $ID_VIRTUALMACHINE --ciuser ansible --cipassword ansible
 qm set $ID_VIRTUALMACHINE --ipconfig0  ip=dhcp
 qm set $ID_VIRTUALMACHINE --machine q35
 qm set $ID_VIRTUALMACHINE --sshkey $CHAVE_PUBLICA
+qm resize $ID_VIRTUALMACHINE scsi0 $TAMANHO_DISCO
 rm -rf $IMAGEM*
